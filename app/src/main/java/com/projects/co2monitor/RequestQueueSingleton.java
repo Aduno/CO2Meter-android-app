bf59e83b011bbdc12f7b5ab8d7accb3b7861d895
Aduno <sanp100d@gmail.com>
2021-12-30 01:24:20 -0500

    implemented singleton pattern

---
+++ app/src/main/java/com/projects/co2monitor/RequestQueueSingleton.java
@@ -0,0 +1,31 @@
+package com.projects.co2monitor;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestQueueSingleton {
+    private static RequestQueueSingleton instance;
+    private RequestQueue reqQueue;
+    private static Context context;
+
+    private RequestQueueSingleton(Context context){
+        this.context = context;
+        reqQueue = getRequestQueue();
+    }
+
+    public static synchronized RequestQueueSingleton getInstance(Context context){
+        if(instance==null){
+            instance = new RequestQueueSingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(reqQueue==null){
+            reqQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return reqQueue;
+    }
+}
